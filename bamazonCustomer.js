4cd225ee61e137a8d2e63ad6d6459750229cab5e
Alexandra M <alexandramatveyeva@yahoo.com>
2018-08-16 22:03:21 -0700

    fixing querry for quantity update after placing the order

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -38,7 +38,7 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   //Select only "name" and "address" from "customers":
-  connection.query("SELECT item_id, product_name FROM products", function (err, result, fields) {
+  connection.query("SELECT item_id, product_name FROM products ORDER BY product_name", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
     runApp();
@@ -51,10 +51,10 @@ function runApp() {
     // prompt users with two messages
     // The first should ask them the ID of the product they would like to buy.
     // The second message should ask how many units of the product they would like to buy.
-	inquirer.prompt([{
+  inquirer.prompt([{
               type: "list",
               name: "action",
-              message: "What item would you like to buy? Choose product 1-10",
+              message: "What item would you like to buy? Choose product id.",
               choices: ['1', '2', '3', '4', '5', '6', '7', '8', '9','10']
           },
           {
@@ -63,23 +63,23 @@ function runApp() {
               message: "How many items would you like to buy?"
           }
 
-      ]).then(function(data) {
+      ]).then(function(order) {
 
 
         //check if our store has enough of the product to meet the customer's request.
         // we retrieve item's information including item quantity
-
-
+            console.log("order.action")
+            console.log(order.action)
             var query = connection.query(
               "SELECT item_id, product_name, price, stock_quantity FROM products WHERE ?", {
-                item_id: data.action
+                item_id: order.action
               },
               function(err, res) {
-                console.log(" product #:"+ data.action  + ", quantity: " + data.quantity + " are chosen!\n");
+                console.log(" product #:"+ order.action  + ", quantity: " + order.quantity + " are chosen!\n");
                 // show the query result:
                 console.log(res);
                 //console.log(res[0].stock_quantity);
-                updateProduct(data, res);
+                updateProduct(order, res);
               }
             );
 
@@ -90,29 +90,29 @@ function runApp() {
 
         
 
-function updateProduct(data, res) {
+function updateProduct(order, res) {
+   
+    var currentQuantity = res[0].stock_quantity;
+    var orderQuantity = order.quantity;
+    var newQuantity = currentQuantity - orderQuantity;
+    console.log(newQuantity);
 
-    
-        var currentQuantity = res[0].stock_quantity;
-        var orderQuantity = data.quantity;
-        var leftQuantity = currentQuantity - orderQuantity;
-        console.log(leftQuantity);
-
-    if (leftQuantity >= 0 && orderQuantity != 0) {
+    if (newQuantity >= 0 && orderQuantity != 0) {
         
         // process the order
         console.log('your order has been placed');
         // update products table
+          
         var query = connection.query(
+            "UPDATE products SET stock_quantity = ? WHERE item_id = ?",
+            [newQuantity, order.action],
 
               // this way does not work ?
               // "UPDATE products SET ? WHERE ?", {
-              //   stock_quantity: '30',
-              //   item_id: '1'
-              // },
+              //   stock_quantity: newQuantity,
+              //   item_id: order.action
+              //  },
 
-              // cannot define leftQuantity
-              "UPDATE products SET stock_quantity = leftQuantity WHERE item_id = '1';",
               function(err, result) {
                 console.log(result);
                 // console.log(result.affectedRows + " product updated!\n"); 
